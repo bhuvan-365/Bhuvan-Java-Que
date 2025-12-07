be5d216c717633173675328b8ef6d87c06f5a976
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2025-12-07 12:16:59 +0545

    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Java to print "Hello World":
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+          System.out.println();
+    }
+}
