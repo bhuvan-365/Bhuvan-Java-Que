366e2216c00debd3feddd963108f6fc1a3bd1ad2
bhuvan-365 <bhuvanbhattarai07@gmail.com>
2025-12-10 14:37:13 +0545

    added

---
+++ Questions-Collection-1/SumOfDigits.java
@@ -0,0 +1,19 @@
+// Write a program to find the sum of digits of a number.
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Sum of digits = " + sum);
+    }
+}
